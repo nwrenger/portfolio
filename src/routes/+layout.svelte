e50b87106682a354ec3262fa27f905c3b579e944
nwrenger <nils@wrenger.net>
2023-12-03 02:58:33 +0100

    :wrench: Fixed purge && add text to icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -20,7 +20,15 @@
 		<!-- App Bar -->
 		<AppBar>
 			<svelte:fragment slot="lead">
-				<img class="h-[40px] max-w-full rounded-sm" src={img} alt="profile" />
+				<a class="sm:!ml-0 w-[40px] sm:w-auto overflow-hidden flex items-center" href="/">
+					<img class="h-[40px] max-w-full rounded-sm" src={img} alt="profile" />
+					<h4 class="h4 ps-3">
+						<span
+							class="bg-gradient-to-br from-blue-500 to-violet-500 bg-clip-text text-transparent box-decoration-clone"
+							>nwrenger</span
+						>
+					</h4>
+				</a>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
 				<LightSwitch />
