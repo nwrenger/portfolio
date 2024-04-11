f28ac7cc640da7132df83d3c8def20eb56d1ae53
nwrenger <nils@wrenger.net>
2024-04-11 21:43:03 +0200

    :bug: CSS fix

--- src/routes/ImageLoader.svelte
+++ src/routes/ImageLoader.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	export let src: string;
 	export let alt = '';
-	$: loaded = false;
+
+	const base = 'w-full h-36 md:h-64 rounded-lg';
+	let loaded = false;
 
 	function handleLoad() {
 		loaded = true;
@@ -9,11 +11,11 @@
 </script>
 
 {#if !loaded}
-	<div class="w-full md:h-64 placeholder rounded-lg animate-pulse" />
+	<div class="{base} placeholder animate-pulse" />
 {/if}
 <img
 	{src}
 	{alt}
 	on:load={handleLoad}
-	class="w-full md:h-64 object-cover rounded-lg shadow-md {!loaded ? 'hidden' : ''}"
+	class="{base} object-cover shadow-md {!loaded ? 'hidden' : ''}"
 />
