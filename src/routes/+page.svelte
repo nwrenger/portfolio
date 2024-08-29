b61597abac02ce2db8d7b04f014a4d98262d0ff0
nwrenger <nils@wrenger.net>
2024-08-29 23:42:43 +0200

    :sparkles: Updated main page text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,16 +10,10 @@
 	<h2>Hi, there!</h2>
 
 	<p>
-		I'm Nils, a high school student passionate about coding, particularly in <a
-			href="https://www.rust-lang.org/"
-			class="link"
-			target="_blank">Rust</a
-		>
-		and in
-		<a href="https://svelte.dev/" class="link" target="_blank">Svelte</a>. I have done a lot of
-		things so far, so look for that under
+		I'm Nils, a high-school <code>Rust</code> enthusiast passionate about coding. I've worked on
+		various projects so far, so look for that under
 		<a href="/projects" class="link">Projects</a>. Feel free to reach out for a chat about coding or
-		anything else via my E-Mail
+		anything else via email at
 		<a href="mailto:nils@wrenger.net" class="link" target="_parent">nils@wrenger.net</a>!
 	</p>
 </div>
@@ -28,25 +22,23 @@
 	<h3>Skills</h3>
 	<p>
 		My focus is on UI development (websites, apps, and TUIs) and performance-driven projects like
-		algorithms and databases. Therefore, my proficiency lies in <code class="code">Rust</code> and
-		in
-		<code class="code">Svelte</code>
-		(i.e., <code class="code">JavaScript</code>/<code class="code">TypeScript</code>). But in the
-		past, I have tried out other programming languages like <code class="code">Go</code> and
-		<code class="code">Python</code>, or even a game engine like
-		<a href="https://godotengine.org/" class="link" target="_blank">Godot</a>, so keep your eyes
-		peeled! Or just check out the list below:
+		algorithms and databases. Therefore, my proficiency lies in <code>Rust</code> and in
+		<code>Svelte</code>
+		(i.e., <code>JavaScript</code> / <code>TypeScript</code>). But in the past, I have tried out
+		other programming languages like <code>Go</code> and
+		<code>Python</code>, or even a game engine like
+		<code>Godot</code>, so keep your eyes peeled! Or just check out the list below:
 	</p>
 	<ul>
-		<li>Rust</li>
-		<li>Svelte</li>
-		<li>Zig</li>
-		<li>Go</li>
-		<li>Godot</li>
-		<li>Python</li>
-		<li>JavaScript/TypeScript</li>
-		<li>HTML & CSS</li>
-		<li>Git & GitHub</li>
+		<li><code>Rust</code></li>
+		<li><code>Svelte</code></li>
+		<li><code>Zig</code></li>
+		<li><code>Go</code></li>
+		<li><code>Godot</code></li>
+		<li><code>Python</code></li>
+		<li><code>JavaScript</code> / <code>TypeScript</code></li>
+		<li><code>HTML</code> & <code>CSS</code></li>
+		<li><code>Git</code> & <code>GitHub</code></li>
 	</ul>
 </div>
 
