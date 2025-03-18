80beb3cd8885ebc53d21391893749d99d49b08e1
Nils Wrenger <nils@wrenger.net>
2025-03-18 21:40:52 +0100

    :sparkles: Improved project counter

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -36,9 +36,9 @@
 
 	<div class="flex items-center justify-between">
 		<h2 class="h2">Projects</h2>
-		<button type="button" class="chip preset-filled-surface-500 md:text-base">
+		<span class="badge preset-filled-surface-500 rounded-full md:text-base">
 			Total: {projects.length}
-		</button>
+		</span>
 	</div>
 
 	{#each years as year}
