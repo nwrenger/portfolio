3b9e5321e51ee65990b77a8f5b3516c44ce47bfd
Nils Wrenger <nils@wrenger.net>
2025-09-26 17:14:18 +0200

    :sparkles: Fixed not found

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,6 +2,8 @@
 	import { page } from '$app/state';
 </script>
 
-<h2 class="h2">
-	{page.status} - {page.error?.message || 'Unknown Error'}
-</h2>
+<div class="mx-auto h-full max-w-3xl px-4 py-6">
+	<h2 class="h2 text-center">
+		{page.status} - {page.error?.message || 'Unknown Error'}
+	</h2>
+</div>
