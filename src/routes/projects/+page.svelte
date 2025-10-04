df9d369cb16d17cd7062d4d39adcaf39a6eb3b02
Nils Wrenger <nils@wrenger.net>
2025-10-04 16:26:12 +0200

    :wrench: Updated heading tag for improved screen readers

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -105,7 +105,7 @@
 										<Link class="stroke-secondary-900-100 size-5 md:size-6" />
 									</a>
 								</div>
-								<h6 class="h6">{summary}</h6>
+								<h4 class="h6">{summary}</h4>
 							</div>
 							<p class="opacity-80">
 								{@html description}
