5f2602fab86354d2b2e5032980e3a59433fbe163
Nils Wrenger <nils@wrenger.net>
2026-03-06 13:04:28 +0100

    :wrench: Forgor redirect

---
+++ src/routes/projects/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import { goto } from '$app/navigation';
+	import { onMount } from 'svelte';
+
+	onMount(() => goto('/#projects'));
+</script>
+
+<div class="mx-auto flex h-full max-w-xl items-center justify-center p-4">
+	<h2 class="h3 font-semibold tracking-tight">Redirecting...</h2>
+</div>
